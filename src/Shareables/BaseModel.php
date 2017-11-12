516942386a4b67de97996e01d824f671e6214377
Rubens Silva <rubens21@gmail.com>
2017-11-12 14:56:15 -0500

    criando sisteam de permissao

--- src/Shareables/BaseModel.php
+++ src/Shareables/BaseModel.php
@@ -10,7 +10,6 @@ namespace CST21\Shareables;
 
 
 use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -19,15 +18,6 @@ use Illuminate\Database\Eloquent\Model;
  * @property int $id
  * @property Carbon $created_at
  * @property Carbon $updated_at
- * @mixin Builder
- * @method static $this find($id, $columns = ['*'])
- * @method static $this findOrFail($id, $columns = ['*'])
- * @method static $this first($columns = ['*'])
- * @method static $this|$this[] get($columns = ['*'])
- * @method static $this|$this[] where($column, $operator = null, $value = null, $boolean = 'and')
- * @method static $this|$this[] orWhere($column, $operator = null, $value = null)
- * @method static $this|$this[] with($relations)
- * @method static $this getQuery()
  */
 abstract class BaseModel extends Model
 {
